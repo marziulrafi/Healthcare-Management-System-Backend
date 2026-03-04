bb5cc9babe4e70e14de0dee8c5dd194fc2e58205
K M Marziul Karim <marziulrafi@gmail.com>
2026-03-04 23:44:43 +0600

    Route, Controller, Service for Specialty Model API

---
+++ src/app/module/specialty/specialty.controller.ts
@@ -0,0 +1,24 @@
+
+import { Request, Response } from "express";
+import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
+import { SpecialtyService } from "./specialty.service";
+
+const createSpecialty = catchAsync(
+    async (req: Request, res: Response) => {
+        const payload = req.body;
+        const result = await SpecialtyService.createSpecialty(payload);
+        sendResponse(res, {
+            httpStatusCode: 201,
+            success: true,
+            message: 'Specialty created successfully',
+            data: result
+        });
+    }
+)
+
+
+
+export const SpecialtyController = {
+    createSpecialty
+}
